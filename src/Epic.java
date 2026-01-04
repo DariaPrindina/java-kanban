52a652a8551a371428ac78df2a8d430bf778920e
Daria Prindina <daria@prindina.ru>
2026-01-04 13:44:33 +0300

    add task, subtask, epic, taskmanager, status files

---
+++ src/Epic.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Epic extends Task{
+    private ArrayList<Integer> subtaskIds = new ArrayList<>();
+
+    public Epic(String name, String description) {
+        super(name, description);
+    }
+
+    public ArrayList<Integer> getSubtaskIds() {
+        return subtaskIds;
+    }
+
+    public void addSubtaskId(int id) {
+        subtaskIds.add(id);
+    }
+
+    public void removeSubtaskId(int id) {
+        subtaskIds.remove((Integer) id);
+    }
+
+    public void clearSubtasks() {
+        subtaskIds.clear();
+    }
+
+    @Override
+    public String toString() {
+        return "Epic{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", status=" + getStatus() +
+                ", subtaskIds=" + subtaskIds +
+                '}';
+    }
+}
